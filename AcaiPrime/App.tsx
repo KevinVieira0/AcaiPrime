a88288968f79477b1adda8d0966607522f78a553
kevin <kevinn.vieira22@gmail.com>
2026-09-23 14:56:56 -0300

    Adição de View e estilização de novas páginas.

--- AcaiPrime/App.tsx
+++ AcaiPrime/App.tsx
@@ -1,8 +1,7 @@
 import { Ionicons } from '@expo/vector-icons';
-import { StatusBar } from 'expo-status-bar';
-import { Image, KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native';
-import { Button } from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
+import { Image, KeyboardAvoidingView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
 
 export default function App() {
   return (
@@ -16,7 +15,7 @@ export default function App() {
             <Text style={styles.subtitleHeader}>O sabor puro da Amazônia</Text>
           </View>
 
-          <View>
+          <View style={styles.icon}>
             <Ionicons name="ice-cream" size={24} color="black" />
           </View>
         </View>
@@ -35,14 +34,42 @@ export default function App() {
           <View>
             <View style={styles.feature}>
               <Image source={require("./assets/acaipreferido.jpg")} style={styles.image}></Image>
-              <Text style={styles.cardTitle}>Açaí Turbinado 500ml</Text>
-              <Text style={styles.cardReference}>Mais Pedido</Text>
+
+
+              <View style={styles.align}>
+                <Text style={styles.cardTitle}>Açaí Turbinado 500ml</Text>
+                <Text style={styles.cardReference}>MAIS PEDIDO</Text>
+              </View>
+
               <Text style={styles.cardDescription}>Açaí puro batido com morango, banana, leite condensado e granola crocante</Text>
-              <Text style={styles.cardPrice}>R$ 22,90</Text>
-              <TouchableOpacity style={styles.addItem}></TouchableOpacity>
+
+              <View style={styles.align}>
+                <Text style={styles.cardPrice}>R$ 22,90</Text>
+
+                <TouchableOpacity style={styles.addItem}>
+
+                  <View style={styles.iconButton}>
+                    <Feather name="shopping-bag" size={14} color="white"
+                      style={{
+                        textShadowColor: '#fff',
+                        textShadowOffset: { width: 0.5, height: 0.5 },
+                        textShadowRadius: 1,
+                      }} />
+                    <View>
+                      <Text style={styles.textButton}>Adicionar</Text>
+                    </View>
+                  </View>
+
+                </TouchableOpacity>
+              </View>
+
             </View>
           </View>
 
+        </View>
+
+        <View>
+          <Text> Nossos Copos e Tigelas</Text>
         </View>
 
         {/*Conteudo*/}
@@ -66,6 +93,11 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "800",
     color: "#2C1B30"
+  },
+  icon: {
+    backgroundColor: "#F3E5F5",
+    borderRadius: 16,
+    padding: 5
   },
   subtitleHeader: {
     fontSize: 14,
@@ -110,13 +142,23 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     borderRadius: 16
   },
+  align: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
   cardTitle: {
     fontSize: 20,
     fontWeight: "800",
     color: "#2f2d2c"
   },
   cardReference: {
-
+    fontSize: 14,
+    fontWeight: "700",
+    paddingHorizontal: 10,
+    borderRadius: 6,
+    backgroundColor: "#F3E5F5",
+    color: "#7B1FA2",
   },
   cardDescription: {
     fontSize: 14,
@@ -126,10 +168,25 @@ const styles = StyleSheet.create({
   cardPrice: {
     fontSize: 20,
     color: "#7B1FA2",
+    flexDirection: "row",
     marginTop: 12,
     fontWeight: "800"
   },
   addItem: {
-
+    backgroundColor: "#7B1FA2",
+    borderRadius: 6,
+    padding: 4,
+  },
+  iconButton: {
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  textButton: {
+    fontSize: 14,
+    fontWeight: "700",
+    color: "#FFFFFF",
+    paddingHorizontal: 6
   },
 });
